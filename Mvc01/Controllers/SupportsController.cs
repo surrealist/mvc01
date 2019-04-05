d4eb554ca4967dbef4103e9442208b12604584e8
Suthep Sangvirotjanaphat <suthep@gfbiz.net>
2019-04-05 14:59:45 +0700

    initial commit

---
+++ Mvc01/Controllers/SupportsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc01.Controllers
+{
+  public class SupportsController : Controller
+  {
+    // GET: Supports
+    public ActionResult Index()
+    {
+      return View(); 
+    }
+
+    public ActionResult Faqs()
+    {
+      return View();
+    }
+
+    public ActionResult Team()
+    {
+      ViewBag.Names = "Suthep,Sukit,Supot";
+      ViewData["Colors"] = "Orange,Lavender,Beige";
+      ViewBag.Key = "Names";
+      return View();
+    }
+
+  }
+}
